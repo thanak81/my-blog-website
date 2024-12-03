97da1c9ab12c12c80211af109ead4c5d3cb6ebef
thanak81 <thanakmech@gmail.com>
2024-12-03 08:53:27 +0700

    change title

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 
 const Blog = () => {
     const post = {
-      title: "Top Car Collection Blog",
+      title: "Top Cat Collection Blog",
       date: "March 15, 2024",
       author: "Thanak Mech",
       readTime: "5 min read",
